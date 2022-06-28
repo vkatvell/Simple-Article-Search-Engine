26f493ca6a6644ad83cdaf99cb15460346ccd50a
vkatvell <vvellanki@smu.edu>
2022-06-28 12:43:21 -0500

    Query processor changes

--- QueryProcessor.h
+++ QueryProcessor.h
@@ -9,21 +9,26 @@
 #include "olestem/stemming/english_stem.h"
 #include <algorithm>
 #include <sstream>
+#include <vector>
 #include "Parser.h"
+
+// rapidJSON headers
+#include "rapidjson/istreamwrapper.h"
+#include "rapidjson/document.h"
 
 class QueryProcessor {
 private:
 
 public:
-    //process the user input queries and search
-    std::string readingQueries(std::string& input); // stem and lowercase queries too
-    //stem the user input
-    std::string stemQuery(const std::string& input);
-    //make the user input lowercase
-    static void toLower(std::string & input);
-    // void printResults(const char* pathtoarticle);
 
-    void menuSystem();
+std::vector<std::string> readingQueries(std::string& input); // stem and lowercase queries too
+
+std::string stemQuery(const std::string& input);
+
+static void toLower(std::string & input);
+// void printResults(const char* pathtoarticle);
+
+void menuSystem();
 
 };
 
